415d313d39f1f8ae7d3987464c203eef1d3e27f1
dwipraj <dwiprajdey33720@gmail.com>
2025-02-28 01:37:05 +0530

    continuing my assingmrnts

---
+++ Q12.js
@@ -0,0 +1,18 @@
+function Message1(){
+   
+       console.log("Open your note book") 
+ 
+}
+function Message2(){
+    setTimeout(() => {
+        console.log("Take your pen")
+    }, 2000);
+}
+
+function Message3(){
+    console.log("Starts writing")
+}
+
+Message1()
+Message2()
+Message3()
