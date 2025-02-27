bfbeb0853954edd3b72828c4f04b3ae966e4f472
dwipraj <dwiprajdey33720@gmail.com>
2025-02-28 01:19:12 +0530

    starting my assingments

---
+++ Q11.js
@@ -0,0 +1,18 @@
+function Task1(){
+    setTimeout(() => {
+        console.log("Wakeing up in the morning")
+    }, 2000);
+}
+function Task2(){
+    setTimeout(()=>{
+        console.log("First Brues youe teeth")
+    },4000)
+}
+function Task3(){
+    setTimeout(() => {
+        console.log("Eat your breakfast")
+    }, 6000);
+}
+Task1()
+Task2()
+Task3()
